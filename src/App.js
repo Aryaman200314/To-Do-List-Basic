fcaba2e93c23de1040929646c1db8084c344875a
Aryaman Sharma <200314arya@gmail.com>
2024-08-31 22:06:10 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Cards from './Components/Cards'
+import './App.css'
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 </style>
 function App() {
   return (
+    <>
     <div>
       <Navbar />
       <Cards />
      
     </div>
+    <p>||made by aryaman Sharma||</p>
+    </>
+    
 
   )
 }
